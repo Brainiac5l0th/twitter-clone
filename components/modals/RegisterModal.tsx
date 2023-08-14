6c9f4932331bc7d5e4b37ee4ccf6186f3c028160
shawonTalukder <shawontalukder.study@gmail.com>
2023-08-15 01:19:18 +0600

    register modal

---
+++ components/modals/RegisterModal.tsx
@@ -0,0 +1,88 @@
+import useLoginModal from "@/hooks/useLoginModal";
+import useRegisterModal from "@/hooks/useRegisterModal";
+import { useCallback, useState } from "react";
+import Input from "../Input";
+import Modal from "../Modal";
+
+const RegisterModal = () => {
+  const loginModal = useLoginModal();
+  const registerModal = useRegisterModal();
+
+  const [email, setEmail] = useState("");
+  const [username, setUsername] = useState("");
+  const [name, setName] = useState("");
+  const [password, setPassword] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
+
+  const handleSubmit = useCallback(async () => {
+    try {
+      setIsLoading(true);
+
+      // @TODO: Add register and login
+
+      registerModal.onClose();
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setIsLoading(false);
+    }
+  }, [registerModal]);
+
+  // main body content for the modal
+  const bodyContent = (
+    <div className="flex flex-col gap-4">
+      <Input
+        placeholder="Email"
+        onChange={(e) => setEmail(e.target.value)}
+        value={email}
+        disabled={isLoading}
+      />
+      <Input
+        placeholder="Name"
+        onChange={(e) => setName(e.target.value)}
+        value={name}
+        disabled={isLoading}
+      />
+      <Input
+        placeholder="Username"
+        onChange={(e) => setUsername(e.target.value)}
+        value={username}
+        disabled={isLoading}
+      />
+      <Input
+        placeholder="password"
+        onChange={(e) => setPassword(e.target.value)}
+        value={password}
+        disabled={isLoading}
+      />
+    </div>
+  );
+
+  // footer content for the modal
+  const footerContent = (
+    <div className="text-neutral-400 text-center mt-4">
+      <p>
+        Already have an account?
+        <span className="text-white cursor-pointer hover:underline">
+          {" "}
+          Sign In
+        </span>
+      </p>
+    </div>
+  );
+
+  return (
+    <Modal
+      disabled={isLoading}
+      isOpen={registerModal.isOpen}
+      title="Create an Account"
+      onClose={registerModal.onClose}
+      onSubmit={handleSubmit}
+      actionLabel="Register"
+      body={bodyContent}
+      footer={footerContent}
+    />
+  );
+};
+
+export default RegisterModal;
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,12 @@
 import Layout from "@/components/Layout";
+import RegisterModal from "@/components/modals/RegisterModal";
 import type { AppProps } from "next/app";
 import "../styles/globals.css";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
+      <RegisterModal />
       <Layout>
         <Component {...pageProps} />
       </Layout>

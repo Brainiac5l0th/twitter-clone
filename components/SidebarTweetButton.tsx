5a4dfc5707eb0a8551f58e24b57296e5c8b0a4a2
shawonTalukder <shawontalukder.study@gmail.com>
2023-08-15 12:19:42 +0600

    changes

--- components/SidebarTweetButton.tsx
+++ components/SidebarTweetButton.tsx
@@ -1,10 +1,20 @@
+import { useCallback } from "react";
+
 import { useRouter } from "next/router";
 import { FaFeather } from "react-icons/fa";
 
+import useLoginModal from "@/hooks/useLoginModal";
+
 const SidebarTweetButton = () => {
   const router = useRouter();
+  const loginModal = useLoginModal();
+
+  const handleToggle = useCallback(() => {
+    loginModal.onOpen();
+  }, [loginModal]);
+
   return (
-    <div onClick={() => router.push("/")}>
+    <div onClick={handleToggle}>
       <div className="mt-6 lg:hidden rounded-full h-14 p-4 w-14 flex items-center justify-center bg-sky-500 hover:bg-opacity-80 cursor-pointer transition">
         <FaFeather size={28} color={"white"} />
       </div>

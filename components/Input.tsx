a955a40bcb76b67da95251ee5a520c8577842a9a
shawonTalukder <shawontalukder.study@gmail.com>
2023-08-16 16:11:42 +0600

    bug fix.. input area text color changed to black

--- components/Input.tsx
+++ components/Input.tsx
@@ -22,7 +22,7 @@ const Input: React.FC<InputProps> = ({
       placeholder={placeholder}
       value={value}
       type={type}
-      className="w-full p-4 text-lg border-2 border-neutral-800 rounded-md outline-none text-white focus:border-sky-500 focus:border-2 transition disabled:bg-neutral-900 disabled:opacity-70 disabled:cursor-not-allowed"
+      className="w-full p-4 text-lg border-2 border-neutral-800 rounded-md outline-none text-black focus:border-sky-500 focus:border-2 transition disabled:bg-neutral-900 disabled:opacity-70 disabled:cursor-not-allowed"
     />
   );
 };

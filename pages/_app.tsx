369957e1df0695a46412b860ddc052689c135aa6
shawonTalukder <shawontalukder.study@gmail.com>
2023-08-18 00:52:04 +0600

    add edit modal to main app

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,15 +2,18 @@ import { SessionProvider } from "next-auth/react";
 import type { AppProps } from "next/app";
 import { Toaster } from "react-hot-toast";
 
+import "../styles/globals.css";
+
 import Layout from "@/components/Layout";
+import EditModal from "@/components/modals/EditModal";
 import LoginModal from "@/components/modals/LoginModal";
 import RegisterModal from "@/components/modals/RegisterModal";
-import "../styles/globals.css";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <SessionProvider session={pageProps.session}>
       <Toaster />
+      <EditModal />
       <LoginModal />
       <RegisterModal />
       <Layout>

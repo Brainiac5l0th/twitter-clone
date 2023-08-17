be232d24ecfe013e40b2f3588780323ed0835692
shawonTalukder <shawontalukder.study@gmail.com>
2023-08-18 02:39:42 +0600

    changes

--- libs/serverAuth.ts
+++ libs/serverAuth.ts
@@ -11,7 +11,7 @@ const serverAuth = async (req: NextApiRequest) => {
   }
 
   const currentUser = await prisma?.user.findUnique({
-    where: { email: session.user.email },
+    where: { email: session.user?.email },
   });
 
   if (!currentUser) {

8c8b3bd1bcff865a41b332ad75948971a68d5c75
shawonTalukder <shawontalukder.study@gmail.com>
2023-08-19 00:29:33 +0600

    changes.. use of getServerSession. getSession is glitchy!

--- libs/serverAuth.ts
+++ libs/serverAuth.ts
@@ -1,17 +1,18 @@
-import { NextApiRequest } from "next";
-import { getSession } from "next-auth/react";
+import { NextApiRequest, NextApiResponse } from "next";
+import { getServerSession } from "next-auth";
 
+import { authOptions } from "@/pages/api/auth/[...nextauth]";
 import prisma from "./prismadb";
 
-const serverAuth = async (req: NextApiRequest) => {
-  const session = await getSession({ req });
+const serverAuth = async (req: NextApiRequest, res: NextApiResponse) => {
+  const session = await getServerSession(req, res, authOptions);
 
   if (!session?.user?.email) {
     throw new Error("Not signed in!");
   }
 
   const currentUser = await prisma?.user.findUnique({
-    where: { email: session.user?.email },
+    where: { email: session.user.email },
   });
 
   if (!currentUser) {

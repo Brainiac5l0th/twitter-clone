b49e6219c221cdeca60133cc5d2ff55c46333362
shawonTalukder <shawontalukder.study@gmail.com>
2023-08-19 19:55:40 +0600

    edit functionality done. User can also update the photo

---
+++ components/ImageUpload.tsx
@@ -0,0 +1,79 @@
+import Image from "next/image";
+import { useCallback, useState } from "react";
+import { useDropzone } from "react-dropzone";
+
+interface ImageUploadProps {
+  label: string;
+  onChange: (base64: string) => void;
+  disabled?: boolean;
+  value?: string;
+}
+
+const ImageUpload: React.FC<ImageUploadProps> = ({
+  onChange,
+  disabled,
+  label,
+  value,
+}) => {
+  //states
+  const [base64, setBase64] = useState(value);
+
+  // handle change handler
+  const handleChange = useCallback(
+    (base64: string) => {
+      onChange(base64);
+    },
+    [onChange]
+  );
+
+  const handleDrop = useCallback(
+    (files: any) => {
+      const file = files[0];
+      const reader = new FileReader();
+
+      reader.onload = (event: any) => {
+        setBase64(event.target.result);
+        handleChange(event.target.result);
+      };
+
+      reader.readAsDataURL(file);
+    },
+    [handleChange]
+  );
+
+  const { getInputProps, getRootProps } = useDropzone({
+    maxFiles: 1,
+    onDrop: handleDrop,
+    disabled,
+    accept: {
+      "image/jpeg": [],
+      "image/jpg": [],
+      "image/png": [],
+    },
+    maxSize: 1000000,
+  });
+  return (
+    <div
+      {...getRootProps({
+        className:
+          "w-full p-4 text-white text-center border-2 border-dotted rounded-md border-neutral-700",
+      })}
+    >
+      <input {...getInputProps()} />
+      {base64 ? (
+        <div className="flex items-center justify-center">
+          <Image
+            alt={"upload Image"}
+            src={base64}
+            height={"100"}
+            width={"100"}
+          />
+        </div>
+      ) : (
+        <p>{label}</p>
+      )}
+    </div>
+  );
+};
+
+export default ImageUpload;
--- components/modals/EditModal.tsx
+++ components/modals/EditModal.tsx
@@ -1,25 +1,115 @@
-import useEditModal from "@/hooks/useEditModal";
+import axios from "axios";
+import { useCallback, useEffect, useState } from "react";
+import { toast } from "react-hot-toast";
 
-import { useCallback, useState } from "react";
+import useCurrentUser from "@/hooks/useCurrentUser";
+import useEditModal from "@/hooks/useEditModal";
+import useUser from "@/hooks/useUser";
+
+import ImageUpload from "../ImageUpload";
+import Input from "../Input";
 import Modal from "../Modal";
 
 const EditModal = () => {
   // hooks
   const editModal = useEditModal();
+  const { data: currentUser } = useCurrentUser();
+  const { mutate: mutateFetchedUser } = useUser(currentUser?.id);
 
   //states
+  const [name, setName] = useState("");
+  const [username, setUsername] = useState("");
+  const [bio, setBio] = useState("");
+  const [profileImage, setProfileImage] = useState("");
+  const [coverImage, setCoverImage] = useState("");
   const [isLoading, setIsLoading] = useState(false);
 
-  //handler
-  const handleSubmit = useCallback(() => {}, []);
+  // EFFECTS
+  useEffect(() => {
+    setName(currentUser?.name);
+    setUsername(currentUser?.username);
+    setBio(currentUser?.bio ? currentUser?.bio : "");
+    setProfileImage(currentUser?.profileImage);
+    setCoverImage(currentUser?.coverImage);
+  }, [currentUser]);
 
+  //handler
+  const handleSubmit = useCallback(async () => {
+    try {
+      setIsLoading(true);
+
+      //update user information
+      await axios.patch("/api/edit", {
+        name,
+        username,
+        profileImage,
+        bio,
+        coverImage,
+      });
+
+      //mutate updated data
+      mutateFetchedUser();
+
+      //send success message
+      toast.success("Information updated successfully!");
+
+      //close the modal
+      editModal.onClose();
+    } catch (error: any) {
+      console.log("editmodal-> line 58", error);
+      toast.error("Something went wrong!");
+    } finally {
+      setIsLoading(false);
+    }
+  }, [bio, name, username, profileImage, coverImage]);
+
+  // body content
+  const bodyContent = (
+    <div className="flex flex-col gap-4">
+      <ImageUpload
+        value={profileImage}
+        onChange={(Image) => setProfileImage(Image)}
+        disabled={isLoading}
+        label={"Upload profile Image"}
+      />
+      <ImageUpload
+        value={coverImage}
+        onChange={(Image) => setCoverImage(Image)}
+        disabled={isLoading}
+        label={"Upload Cover Image"}
+      />
+      <Input
+        placeholder="Name"
+        onChange={(e) => setName(e.target.value)}
+        disabled={isLoading}
+        type="text"
+        value={name}
+      />
+      <Input
+        placeholder="Username"
+        onChange={(e) => setUsername(e.target.value)}
+        disabled={isLoading}
+        type="text"
+        value={username}
+      />
+      <Input
+        placeholder="Bio"
+        onChange={(e) => setBio(e.target.value)}
+        disabled={isLoading}
+        type="text"
+        value={bio}
+      />
+    </div>
+  );
   return (
     <Modal
-      isOpen={editModal.isOpen}
       disabled={isLoading}
-      actionLabel=""
+      isOpen={editModal.isOpen}
+      actionLabel="Save"
+      title="Edit Your Profile"
       onClose={editModal.onClose}
       onSubmit={handleSubmit}
+      body={bodyContent}
     />
   );
 };

ad656d3203960b1d7e29db545611cbb0915ea8fa
shawonTalukder <shawontalukder.study@gmail.com>
2023-08-20 01:36:57 +0600

    postfeed on the home buttton!

---
+++ components/posts/PostFeed.tsx
@@ -0,0 +1,19 @@
+import usePosts from "@/hooks/usePosts";
+import PostItem from "./PostItem";
+
+interface PostFeedProps {
+  userid?: string;
+}
+
+const PostFeed: React.FC<PostFeedProps> = ({ userid }) => {
+  const { data: posts = [] } = usePosts();
+  return (
+    <>
+      {posts.map((post: Record<string, any>) => (
+        <PostItem userid={userid} key={post?.id} data={post} />
+      ))}
+    </>
+  );
+};
+
+export default PostFeed;

5a0691bdcedb86ead249cc5147c2d0810aabfdad
shawonTalukder <shawontalukder.study@gmail.com>
2023-08-21 16:42:45 +0600

    hook to fetch post details

---
+++ hooks/usePost.ts
@@ -0,0 +1,18 @@
+import useSWR from "swr";
+
+import fetcher from "@/libs/fetcher";
+
+const usePost = (postId: string) => {
+  const url = postId ? `/api/posts/${postId}` : null;
+
+  const { data, isLoading, error, mutate } = useSWR(url, fetcher);
+
+  return {
+    data,
+    error,
+    isLoading,
+    mutate,
+  };
+};
+
+export default usePost;

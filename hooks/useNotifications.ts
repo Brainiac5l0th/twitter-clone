c2c16ebdac1a66ff81af3f508ca50aad85322286
shawonTalukder <shawontalukder.study@gmail.com>
2023-08-22 14:55:30 +0600

    bug fix

--- hooks/useNotifications.ts
+++ hooks/useNotifications.ts
@@ -2,10 +2,12 @@ import useSWR from "swr";
 
 import fetcher from "@/libs/fetcher";
 
-const useNotifications = async (userId?: string) => {
+const useNotifications = (userId?: string) => {
   const url = userId ? `/api/notifications/${userId}` : null;
 
   const { data, mutate, isLoading, error } = useSWR(url, fetcher);
 
   return { data, mutate, isLoading, error };
 };
+
+export default useNotifications;
